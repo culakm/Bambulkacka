4598554dce528f25648bc60174ead75ffbc13213
culakm <culak.martin@gmail.com>
2015-10-26 21:23:05 +0100

    funkcne pred implementaciou settingsov

--- app/src/main/java/com/hellbilling/bambulkacka/MainActivity.java
+++ app/src/main/java/com/hellbilling/bambulkacka/MainActivity.java
@@ -1,8 +1,6 @@
 package com.hellbilling.bambulkacka;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,13 +9,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.RadioButton;
-import android.widget.Toast;
 
 import java.lang.reflect.Method;
 
 public class MainActivity extends ActionBarActivity {
-
-
 
     /*
 
@@ -39,8 +34,9 @@ public class MainActivity extends ActionBarActivity {
 
     po splneni urciteho poctu prikladov spustit youtube
 
-
-
+    pouzit custom preference, posuvace, hodiny, kalendar,,,,,,
+    http://developer.android.com/guide/topics/ui/settings.html#Custom
+    http://stackoverflow.com/questions/16108609/android-creating-custom-preference
 
     co je to context??
      */
--- app/src/main/java/com/hellbilling/bambulkacka/SettingsActivity.java
+++ app/src/main/java/com/hellbilling/bambulkacka/SettingsActivity.java
@@ -1,11 +1,7 @@
 package com.hellbilling.bambulkacka;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class SettingsActivity extends Activity {
     @Override
